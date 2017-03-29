c92cf2aee85e2dbd643d669747359796c9842a3e
Miloszex <milosz.weda@o2.pl>
2017-03-29 20:25:23 +0200

    naprawione dodawanie

--- account/views.py
+++ account/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 
+# def index(request):
+#
+#     return render(request, 'account/index.html')
+
 def register(request):
 
     if request.method == 'POST':
