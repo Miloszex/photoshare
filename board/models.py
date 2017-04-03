ecb8569bab9f8c7345161c8be488fe45879c330d
Miloszex <milosz.weda@o2.pl>
2017-04-03 15:50:10 +0200

    dodawanie wyswietlanie zdjec na tablicy staly rozmiar do poprawy

--- board/models.py
+++ board/models.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Image(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField()
     description = models.TextField()
     likes = models.IntegerField(default=0)
     reports = models.IntegerField(default=0)
+
