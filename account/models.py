98bcfb7ad6d9fc600ac4f3c75069586376cf57e3
Miloszex <milosz.weda@o2.pl>
2017-04-06 22:06:45 +0200

    relacja obserwowania

--- account/models.py
+++ account/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField()
+
+class Observation(models.Model):
+    subject = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subject')
+    target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target')
