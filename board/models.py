2d853a395efb72b8b4484a582a4ba7435234d56c
Miloszex <milosz.weda@o2.pl>
2017-04-07 10:14:52 +0200

    wyczyszczone media i database dodany ordering

--- board/models.py
+++ board/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 
 class Image(models.Model):
@@ -10,4 +11,7 @@ class Image(models.Model):
     description = models.TextField()
     likes = models.IntegerField(default=0)
     reports = models.IntegerField(default=0)
+    add_date = models.DateField(default=now())
 
+    class Meta:
+        ordering = ['-add_date']
